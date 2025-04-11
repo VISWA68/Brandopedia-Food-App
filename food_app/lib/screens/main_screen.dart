b20bed9dcd87ad5e7d6953c93da591a5f992afc8
VISWA68 <you@example.com>
2025-04-12 00:18:58 +0530

    Modified the UI

--- food_app/lib/screens/main_screen.dart
+++ food_app/lib/screens/main_screen.dart
@@ -13,7 +13,7 @@ class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
 
   final List<Widget> _screens = [
-    const HomeScreen(),
+    HomeScreen(),
     const CartScreen(),
   ];
 
